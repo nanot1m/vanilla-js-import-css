b427bba974dd363f1bd1e0a8960736081a32ce5b
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-01-11 23:11:35 +0500

    extracted resolvePath function

--- require-css/require-css.js
+++ require-css/require-css.js
@@ -1,8 +1,6 @@
 const styles = new Set();
 
 export function requireCss(meta) {
-  const modulePathParts = meta.url.split("/").slice(0, -1);
-
   const module = {
     add
   };
@@ -10,21 +8,7 @@ export function requireCss(meta) {
   return module;
 
   function add(path) {
-    const relativePathParts = path.split("../").map(x => x.replace("./", ""));
-    const resultPathParts = modulePathParts.slice();
-    for (let part of relativePathParts) {
-      if (part === "") {
-        const popped = resultPathParts.pop();
-        if (popped === undefined) {
-          throw new Error("Wrong path specified: " + path);
-        }
-      } else {
-        resultPathParts.push(part);
-      }
-    }
-    const resultPath = resultPathParts.join("/");
-    styles.add(resultPath);
-    console.log(...styles);
+    styles.add(resolvePath(meta.url, path));
     return module;
   }
 }
@@ -36,4 +20,21 @@ export async function injectCss() {
   const style = document.createElement("style");
   style.appendChild(document.createTextNode(styleSheet));
   document.head.appendChild(style);
+}
+
+function resolvePath(from, to) {
+  const fromPathParts = from.split("/").slice(0, -1);
+  const toPathParts = to.split("../").map(x => x.replace("./", ""));
+  const resultPathParts = fromPathParts.slice();
+  for (let part of toPathParts) {
+    if (part === "") {
+      const popped = resultPathParts.pop();
+      if (popped === undefined) {
+        throw new Error("Wrong path specified: " + path);
+      }
+    } else {
+      resultPathParts.push(part);
+    }
+  }
+  return resultPathParts.join("/");
 }
